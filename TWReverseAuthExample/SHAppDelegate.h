089f8daf8ce5e51ac6ea4e9dad70ce459a13c8c3
Seivan Heidari <seivan@kth.se>
2013-03-13 12:55:26 +0100

    Initial Commit

---
+++ TWReverseAuthExample/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  TWReverseAuthExample
+//
+//  Created by Seivan Heidari on 3/13/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TWReverseAuthExample/SHViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHViewController.h
+//  TWReverseAuthExample
+//
+//  Created by Seivan Heidari on 3/13/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHViewController : UIViewController
+
+@end
